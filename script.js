7a060ed4de49f5bfb2c2051cdc89b718e4e128e8
NemanjaJovanovicNJ <jovanovic.p.nemanja@gmail.com>
2022-12-29 12:34:50 +0100

    Partially fixed collapsing

--- script.js
+++ script.js
@@ -3,65 +3,33 @@
 const questions = document.querySelectorAll(".question");
 
 questions.forEach((question) => {
-  const button = question.querySelector(".btn");
-  const answer = question.querySelector(".answer");
-  const questionTextEl = question.querySelector(".question-text");
-  const arrowEl = question.querySelector(".arrow");
-
-  let active = true;
-  button.addEventListener("click", function () {
+  let active = 0;
+  question.addEventListener("click", function () {
     questions.forEach((item) => {
-      const display = function () {
-        item.querySelector(".question-text").style.fontWeight = "bold";
-        item.querySelector(".answer").classList.remove("hidden");
-        item.querySelector(".arrow").style.transform = "rotate(180deg)";
+      const answer = item.querySelector(".answer");
+      const questionTextEl = item.querySelector(".question-text");
+      const arrowEl = item.querySelector(".arrow");
+      const display = () => {
+        answer.classList.remove("hidden");
+        questionTextEl.style.fontWeight = "bold";
+        arrowEl.style.transform = "rotate(180deg)";
+      };
+      const hide = () => {
+        answer.classList.add("hidden");
+        questionTextEl.style.fontWeight = "400";
+        arrowEl.style.transform = "rotate(0deg)";
       };
 
-      const reset = function () {
-        item.querySelector(".question-text").style.fontWeight = "400";
-        item.querySelector(".answer").classList.add("hidden");
-        item.querySelector(".arrow").style.transform = "rotate(0deg)";
-      };
-
-      if (item == question) {
-        if (active == false) {
-          display();
-          active = true;
-        } else {
-          reset();
-          active = false;
-        }
+      if (question !== item) {
+        hide();
       } else {
-        active = false;
-        reset();
-      }
-    });
-  });
-  questionTextEl.addEventListener("click", function () {
-    questions.forEach((item) => {
-      const display = function () {
-        item.querySelector(".question-text").style.fontWeight = "bold";
-        item.querySelector(".answer").classList.remove("hidden");
-        item.querySelector(".arrow").style.transform = "rotate(180deg)";
-      };
-
-      const reset = function () {
-        item.querySelector(".question-text").style.fontWeight = "400";
-        item.querySelector(".answer").classList.add("hidden");
-        item.querySelector(".arrow").style.transform = "rotate(0deg)";
-      };
-
-      if (item == question) {
-        if (active == false) {
+        if (active == 0) {
           display();
-          active = true;
+          active = 1;
         } else {
-          reset();
-          active = false;
+          hide();
+          active = 0;
         }
-      } else {
-        active = false;
-        reset();
       }
     });
   });
