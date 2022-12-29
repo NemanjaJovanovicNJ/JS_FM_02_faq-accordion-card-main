127444b09ca0321c8df68a0e0b60c94cc02894dd
NemanjaJovanovicNJ <jovanovic.p.nemanja@gmail.com>
2022-12-29 14:01:06 +0100

    Final version

--- script.js
+++ script.js
@@ -3,32 +3,29 @@
 const questions = document.querySelectorAll(".question");
 
 questions.forEach((question) => {
-  let active = 0;
   question.addEventListener("click", function () {
-    questions.forEach((item) => {
+    questions.forEach((item, i) => {
       const answer = item.querySelector(".answer");
       const questionTextEl = item.querySelector(".question-text");
       const arrowEl = item.querySelector(".arrow");
       const display = () => {
-        answer.classList.remove("hidden");
         questionTextEl.style.fontWeight = "bold";
         arrowEl.style.transform = "rotate(180deg)";
       };
       const hide = () => {
-        answer.classList.add("hidden");
         questionTextEl.style.fontWeight = "400";
         arrowEl.style.transform = "rotate(0deg)";
       };
 
       if (question !== item) {
+        answer.classList.add("hidden");
         hide();
       } else {
-        if (active == 0) {
+        answer.classList.toggle("hidden");
+        if (!answer.classList.contains("hidden")) {
           display();
-          active = 1;
         } else {
           hide();
-          active = 0;
         }
       }
     });
