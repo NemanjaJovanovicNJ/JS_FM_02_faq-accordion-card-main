b879b64de505e92d124eccd966479cb6adbb455a
NemanjaJovanovicNJ <jovanovic.p.nemanja@gmail.com>
2023-01-08 21:53:02 +0100

    Fix issue with closing accordion elements

--- script.js
+++ script.js
@@ -2,32 +2,31 @@
 
 const questions = document.querySelectorAll(".question");
 
+const display = (questionTextEl, arrowEl) => {
+  questionTextEl.style.fontWeight = "bold";
+  arrowEl.style.transform = "rotate(180deg)";
+};
+const hide = (questionTextEl, arrowEl) => {
+  questionTextEl.style.fontWeight = "400";
+  arrowEl.style.transform = "rotate(0deg)";
+};
+
 questions.forEach((question) => {
   question.addEventListener("click", function () {
-    questions.forEach((item, i) => {
+    questions.forEach((item) => {
       const answer = item.querySelector(".answer");
       const questionTextEl = item.querySelector(".question-text");
       const arrowEl = item.querySelector(".arrow");
-      const display = () => {
-        questionTextEl.style.fontWeight = "bold";
-        arrowEl.style.transform = "rotate(180deg)";
-      };
-      const hide = () => {
-        questionTextEl.style.fontWeight = "400";
-        arrowEl.style.transform = "rotate(0deg)";
-      };
 
       if (question !== item) {
         answer.classList.add("hidden");
-        hide();
-      } else {
-        answer.classList.toggle("hidden");
-        if (!answer.classList.contains("hidden")) {
-          display();
-        } else {
-          hide();
-        }
+        hide(questionTextEl, arrowEl);
+        return;
       }
+      answer.classList.toggle("hidden");
+      answer.classList.contains("hidden")
+        ? hide(questionTextEl, arrowEl)
+        : display(questionTextEl, arrowEl);
     });
   });
 });
